cae2351e9de1f891d3ecacd85bf245c633dfd8af
Starboihacks369 <104632716+Starboihacks369@users.noreply.github.com>
2022-06-14 07:17:11 +0530

    Update source.py

--- FallenRobot/modules/source.py
+++ FallenRobot/modules/source.py
@@ -7,29 +7,28 @@ from pyrogram import filters
 from FallenRobot import pbot as client
 
 
-ANON = "https://telegra.ph/file/89639083c2100561c7276.jpg"
+ANON = "https://telegra.ph/file/26b5ac4684af8c68ebdce.jpg"
 
 @client.on_message(filters.command(["repo", "source"]))
 async def repo(client, message):
     await message.reply_photo(
         photo=ANON,
-        caption=f"""**ʜᴇʏ​ {message.from_user.mention()},\n\nɪ ᴀᴍ [𝙰𝙰𝚁𝚄 ✘ 𝚁𝙾𝙱𝙾𝚃-🇮🇳](t.me/AaRu_X_RoBoT)**
+        caption=f"""**ʜᴇʏ​ {message.from_user.mention()},\n\nɪ ᴀᴍ [𝚂𝚃𝙰𝚁✘𝙱𝙾𝙸-🇮🇳](t.me/Itz_star_robot)**
 
-**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [❛-𝐌𝐑'𝐁𝐀𝐍𝐍𝐀-𝐱𝐃](tg://user?id=5150456401)
+**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [𝐒𝐓𝐀𝐑🇽𝐁𝐎𝐈](tg://user?id=5470068285)
 **» ᴩʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{y()}`
 **» ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{o}` 
 **» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
 **» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{z}`
 
-**ᴀᴀʀᴜ ✘ ʀᴏʙᴏᴛ sᴏᴜʀᴄᴇ ɪs ɴᴏᴡ ᴩᴜʙʟɪᴄ ᴀɴᴅ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ.**
+**sᴛᴀʀ ✘ ʀᴏʙᴏᴛ sᴏᴜʀᴄᴇ ɪs ɴᴏᴡ ᴩᴜʙʟɪᴄ ᴀɴᴅ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ.**
 """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "• ᴏᴡɴᴇʀ •", url="tg://user?id=5150456401"), 
-                    InlineKeyboardButton(
-                        "• sᴏᴜʀᴄᴇ •", url="https://github.com/BANNA-XD143/AARUROBOT")
+                        "• ᴏᴡɴᴇʀ •", url="tg://user?id=5470068285"), 
+                    InlineKeyboardButton(https://github.com/Starboihacks369/STARROBOThttps://github.com/Starboihacks369/STARROBOT")
                 ]
             ]
         )
