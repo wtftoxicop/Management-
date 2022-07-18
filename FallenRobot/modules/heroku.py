bcf3d79826e8f22d3efabf297328e2bffb9ee24f
Star boi <104632716+Starboihacks369@users.noreply.github.com>
2022-07-18 10:03:39 +0530

    Update heroku.py

--- FallenRobot/modules/heroku.py
+++ FallenRobot/modules/heroku.py
@@ -191,7 +191,7 @@ async def _(dyno):
         dyno.chat_id,
         "logs.txt",
         reply_to=dyno.id,
-        caption="Fallen Bot Logs.",
+        caption="Star Robot Logs.",
     )
 
     await asyncio.sleep(5)
