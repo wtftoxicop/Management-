cfd009f8650ef3723e97320d9c9343f349523d7e
sᴛᴀʀ ʙᴏɪ <104632716+Starboihacks369@users.noreply.github.com>
2022-08-06 11:21:43 +0530

    Update blacklistusers.py

--- FallenRobot/modules/blacklistusers.py
+++ FallenRobot/modules/blacklistusers.py
@@ -165,5 +165,5 @@ dispatcher.add_handler(BL_HANDLER)
 dispatcher.add_handler(UNBL_HANDLER)
 dispatcher.add_handler(BLUSERS_HANDLER)
 
-__mod_name__ = "Blacklisting Users"
+__mod_name__ = "📛ʙʟᴀᴄᴋʟɪsᴛ ᴜsᴇʀ"
 __handlers__ = [BL_HANDLER, UNBL_HANDLER, BLUSERS_HANDLER]
