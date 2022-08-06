dd45811a16647d087a099c69bce7623bbcb2e3e9
sᴛᴀʀ ʙᴏɪ <104632716+Starboihacks369@users.noreply.github.com>
2022-08-06 12:07:04 +0530

    Update gitinfo.py

--- FallenRobot/modules/gitinfo.py
+++ FallenRobot/modules/gitinfo.py
@@ -5,7 +5,7 @@ from FallenRobot import pbot
 from FallenRobot.pyrogramee.errors import capture_err
 
 
-__mod_name__ = "Gɪᴛʜᴜʙ"
+__mod_name__ = "👻ɢɪᴛʜᴜʙ"
 
 __help__ = """
 I will give information about github profile 
